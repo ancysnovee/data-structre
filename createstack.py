5e74ee7f1834e3a56fa713c51d591123aacb0f0d
ancysnovee <ancysnoveem@gmail.com>
2025-03-19 15:55:08 +0530

    updated

--- createstack.py
+++ createstack.py
@@ -8,3 +8,15 @@ if __name__=='__main__':
 
     print(stack())
     
+
+def stack_pop():
+    items=[45,90,66,32]
+    items.pop(0)
+    items.pop(0)
+    return items
+
+if __name__=='__main__':
+
+    print(stack_pop())
+    
+
--- testfile.py
+++ testfile.py
@@ -2,6 +2,7 @@ from createstack import stack
 
 def testcase():
     assert(stack()) == [45, 66]
+    assert(stack_pop()) == [66,32]
 
 
 
